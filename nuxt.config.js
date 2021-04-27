11a4b9dc066f41b54de161f865b89be950245d9f
jalofernandez <jalofernandez@gmail.com>
2021-04-27 12:22:28 +0200

    feat: color mode nuxtjs module to change color theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,8 @@ export default {
     '@nuxtjs/stylelint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    // https://color-mode.nuxtjs.org/
+    '@nuxtjs/color-mode',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
