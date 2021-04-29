0bfc21995b234c3e40810e3f587ef513ee80a1ea
jalofernandez <jalofernandez@gmail.com>
2021-04-29 18:38:49 +0200

    fix: useless css code removed

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,9 +20,9 @@ html {
   box-sizing: border-box;
 }
 
-html.dark {
+/* html.dark {
   @apply bg-gray-700;
-}
+} */
 
 html.dark,
 html.light {
@@ -38,34 +38,5 @@ html.light {
 *::after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
