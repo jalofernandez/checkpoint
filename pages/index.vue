2c4772fc2417606f43c15667af3769a16a328212
jalofernandez <jalofernandez@gmail.com>
2021-05-07 18:06:59 +0200

    feat: index page with design and navigation improves

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <section class="container">
-      <!-- component -->
+    <!-- <section class="container">
       <div
         class="bg-gray-100 m-auto w-96 h-64 mt-5"
         :style="`
@@ -91,7 +90,7 @@
           </div>
         </div>
       </div>
-    </section>
+    </section> -->
     <section class="w-full bg-gray-100 dark:bg-gray-800 rounded-t-full">
       <div class="container">
         <!-- component -->
@@ -100,35 +99,17 @@
             class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3"
           >
             <!-- 1 card -->
-            <article
-              class="relative bg-white py-6 px-6 rounded-2xl w-64 my-4 shadow-xl"
-            >
-              <div
-                class="text-white flex items-center absolute rounded-full py-4 px-4 shadow-xl bg-pink-500 left-4 -top-6"
+            <NuxtLink to="/canessa/customers">
+              <article
+                class="relative bg-white py-6 px-6 rounded-2xl w-64 my-4 shadow-xl"
               >
-                <!-- svg  -->
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-8 w-8"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
+                <div
+                  class="text-white flex items-center absolute rounded-full py-4 px-4 shadow-xl bg-pink-500 left-4 -top-6"
                 >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"
-                  />
-                </svg>
-              </div>
-              <div class="mt-8">
-                <p class="text-xl font-semibold my-2">App Development</p>
-                <div class="flex space-x-2 text-gray-400 text-sm">
                   <!-- svg  -->
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5"
+                    class="h-8 w-8"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -137,67 +118,87 @@
                       stroke-linecap="round"
                       stroke-linejoin="round"
                       stroke-width="2"
-                      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                    />
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                      d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"
                     />
                   </svg>
-                  <p>Marketing Team</p>
                 </div>
-                <div class="flex space-x-2 text-gray-400 text-sm my-3">
-                  <!-- svg  -->
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-                    />
-                  </svg>
-                  <p>1 Weeks Left</p>
-                </div>
-                <div class="border-t-2"></div>
+                <div class="mt-8">
+                  <p class="text-xl font-semibold my-2">Canessa</p>
+                  <div class="flex space-x-2 text-gray-400 text-sm">
+                    <!-- svg  -->
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                      />
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                    </svg>
+                    <p>Marketing Team</p>
+                  </div>
+                  <div class="flex space-x-2 text-gray-400 text-sm my-3">
+                    <!-- svg  -->
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                      />
+                    </svg>
+                    <p>1 Weeks Left</p>
+                  </div>
+                  <div class="border-t-2"></div>
 
-                <div class="flex justify-between">
-                  <div class="my-2">
-                    <p class="font-semibold text-base mb-2">Team Member</p>
-                    <div class="flex space-x-2">
-                      <img
-                        src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                        class="w-6 h-6 rounded-full"
-                      />
-                      <img
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxSqK0tVELGWDYAiUY1oRrfnGJCKSKv95OGUtm9eKG9HQLn769YDujQi1QFat32xl-BiY&usqp=CAU"
-                        class="w-6 h-6 rounded-full"
-                      />
+                  <div class="flex justify-between">
+                    <div class="my-2">
+                      <p class="font-semibold text-base mb-2">Team Member</p>
+                      <div class="flex space-x-2">
+                        <img
+                          src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                          class="w-6 h-6 rounded-full"
+                        />
+                        <img
+                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxSqK0tVELGWDYAiUY1oRrfnGJCKSKv95OGUtm9eKG9HQLn769YDujQi1QFat32xl-BiY&usqp=CAU"
+                          class="w-6 h-6 rounded-full"
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div class="my-2">
-                    <p class="font-semibold text-base mb-2">Progress</p>
-                    <div class="text-base text-gray-400 font-semibold">
-                      <p>34%</p>
+                    <div class="my-2">
+                      <p class="font-semibold text-base mb-2">Progress</p>
+                      <div class="text-base text-gray-400 font-semibold">
+                        <p>34%</p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </article>
+              </article>
+            </NuxtLink>
           </div>
         </div>
       </div>
     </section>
-    <section class="container">
+    <!-- <section class="container">
       <BaseLink />
-    </section>
+    </section> -->
   </div>
 </template>
 
